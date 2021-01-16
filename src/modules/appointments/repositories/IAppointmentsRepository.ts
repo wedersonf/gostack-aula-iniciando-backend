e2b4d8c9d7b057816bb114eafd71cd53e2097def
wedersonf <wedymf@hotmail.com>
2021-01-16 19:28:43 -0300

    reescrevendo repositorios

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
+import ICreateAppoitmentDTO from '../dtos/ICreateAppoitmentDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppoitmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -25,12 +25,10 @@ class createAppointmentService {
       throw new AppError('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
