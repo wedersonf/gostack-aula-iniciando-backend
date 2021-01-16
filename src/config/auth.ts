1ab661c4ac988e472407ce4add3bdca3158e7f70
wedersonf <wedymf@hotmail.com>
2021-01-16 18:14:52 -0300

    Initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '1a6e0f6c35e581cc7ed31677f4beeb60',
+    expiresIn: '1d',
+  },
+};
---
+++ src/shared/routes/users.routes.ts
@@ -0,0 +1,60 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+const UsersRouter = Router();
+const upload = multer(uploadConfig);
+
+UsersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+
+  const createUser = new CreateUserService();
+
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  const userWithoutPassoword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
+
+  return response.json(userWithoutPassoword);
+});
+
+UsersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  },
+);
+
+export default UsersRouter;
