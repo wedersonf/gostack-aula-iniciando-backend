7952c56ee7bba8b750589207c515aaa4c9d263ec
wedersonf <wedymf@hotmail.com>
2021-01-16 19:00:38 -0300

    organized by responsibity

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,10 +2,10 @@ import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
-import upload from '../config/upload';
-import AppError from '../errors/AppError';
+import upload from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import User from '../models/User';
+import User from '../infra/typeorm/entities/User';
 
 interface Request {
   user_id: string;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,10 +3,11 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import upload from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import './database';
-import upload from './config/upload';
-import AppError from './errors/AppError';
+
+import '@shared/infra/typeorm';
 
 const app = express();
 
