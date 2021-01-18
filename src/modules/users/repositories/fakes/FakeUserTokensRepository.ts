378cf79c642c74926fdb1577165617cc0bfec078
wedersonf <wedymf@hotmail.com>
2021-01-17 23:16:13 -0300

    reset de senha

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -10,9 +10,21 @@ class FakeUserTokensRepository implements IUserTokensRepository {
   public async generate(user_id: string): Promise<UserToken> {
     const userToken = new UserToken();
 
-    Object.assign(userToken, { id: uuid(), token: uuid(), user_id });
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
 
     return userToken;
   }
