6a5eee115ead159899d693fd4e1951407075519e
wedersonf <wedymf@hotmail.com>
2021-01-24 00:03:36 -0300

    alterando a rota de reset de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -50,7 +50,7 @@ class SendForgotPasswordEmailService {
         file: forgotPassowordTemplate,
         variables: {
           name: user.name,
-          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+          link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
         },
       },
     });
