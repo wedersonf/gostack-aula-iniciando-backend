84bcfef862fc9290828b25de24c66a7eaaf5ed97
wedersonf <wedymf@hotmail.com>
2021-01-23 22:12:09 -0300

    corrigindo agendamentos no mesmo horario

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -50,6 +50,7 @@ class createAppointmentService {
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
+      provider_id,
     );
 
     if (findAppointmentInSameDate) {
